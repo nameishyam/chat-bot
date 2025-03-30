02ea81f35074d657a1d3de7fbd1ad4b32e76e2cd
Geddam Shyam Gowtham <geddamgowtham04@outlook.com>
2025-03-30 15:27:48 +0530

    modified vite.config.js file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://chat-bot-eight-nu.vercel.app/",
+  server: {
+    host: "0.0.0.0", // Ensure it listens on all interfaces
+    port: parseInt(import.meta.env.VITE_PORT) || 5173, // Use the VITE_PORT from environment or fallback
+  },
 });
