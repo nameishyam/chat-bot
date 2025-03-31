09d1723314aec63f9fd076c3aab2cc57b65b3adf
Geddam Shyam Gowtham <geddamgowtham04@outlook.com>
2025-03-31 14:02:00 +0530

    modified vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0", // Ensure it listens on all interfaces
-    port: parseInt(import.meta.env.VITE_PORT) || 5173, // Use the VITE_PORT from environment or fallback
-  },
 });
